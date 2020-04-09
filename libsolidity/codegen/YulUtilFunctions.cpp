f2b475e2c56ce2c4516b70502bf6d1c16f61bba0
chriseth <chris@ethereum.org>
2020-04-09 15:06:26 +0200

    Simplify decode error message function.

--- libsolidity/codegen/YulUtilFunctions.cpp
+++ libsolidity/codegen/YulUtilFunctions.cpp
@@ -2258,58 +2258,42 @@ string YulUtilFunctions::tryDecodeErrorMessageFunction()
 	string const functionName = "try_decode_error_message";
 
 	return m_functionCollector.createFunction(functionName, [&]() {
-		string const errorHash = FixedHash<4>(util::keccak256("Error(string)")).hex();
 		return util::Whiskers(R"(
-			function <functionName>() -> data {
-				data := mload(<freeMemoryPointer>)
-
-				if lt(returndatasize(), 0x44) {
-					data := 0
-					leave
-				}
+			function <functionName>() -> ret {
+				if lt(returndatasize(), 0x44) { leave }
 
 				returndatacopy(0, 0, 4)
-				let sig := <getSig>
-				if iszero(eq(sig, 0x<ErrorSignature>)) {
-					data := 0
-					leave
-				}
+				let sig := <shr224>(mload(0))
+				if iszero(eq(sig, 0x<ErrorSignature>)) { leave }
 
+				let data := mload(<freeMemoryPointer>)
 				returndatacopy(data, 4, sub(returndatasize(), 4))
+
 				let offset := mload(data)
 				if or(
 					gt(offset, 0xffffffffffffffff),
 					gt(add(offset, 0x24), returndatasize())
 				) {
-					data := 0
 					leave
 				}
 
 				let msg := add(data, offset)
 				let length := mload(msg)
-				if gt(length, 0xffffffffffffffff) {
-					data := 0
-					leave
-				}
+				if gt(length, 0xffffffffffffffff) { leave }
 
 				let end := add(add(msg, 0x20), length)
-				if gt(end, add(data, returndatasize())) {
-					data := 0
-					leave
-				}
+				if gt(end, add(data, returndatasize())) { leave }
 
-				mstore(<freeMemoryPointer>, and(add(end, 0x1f), not(0x1f)))
-				data := msg
+				mstore(<freeMemoryPointer>, add(add(msg, 0x20), <roundUp>(length)))
+				ret := msg
 			}
 		)")
-		("ErrorSignature", errorHash)
 		("functionName", functionName)
+		("shr224", shiftRightFunction(224))
+		("ErrorSignature", FixedHash<4>(util::keccak256("Error(string)")).hex())
 		("freeMemoryPointer", to_string(CompilerUtils::freeMemoryPointer))
-		("getSig",
-			m_evmVersion.hasBitwiseShifting() ?
-			"shr(224, mload(0))" :
-			"div(mload(0), " + (u256(1) << 224).str() + ")"
-		).render();
+		("roundUp", roundUpFunction())
+		.render();
 	});
 }
 
